11c9efd947c2b2003e8ae37362d47c9f0ff4c988
Edward <eddardpearce@gmail.com>
2017-09-12 23:13:57 +0100

    Map generation nearly done, needs some bug fixes.

--- mapGen.java
+++ mapGen.java
@@ -1,24 +1,101 @@
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class mapGen{
-    int[][] spawnRoom=getSpawnRoom;
-    int[][] exitRoom=getExitRoom;
-    
+
+    public static int mapX=0;
+    public static int mapY=0;
+    static String[][] mapGrid;
     public static void main(String[] args){
-        int mapX=0;
-        int mapY=0;
-        Random rn=new Random();
-        mapX=rn.nextInt(3)+7;
-        mapY=rn.nextInt(3)+7;
-        int[][] mapGrid=new int[mapX][mapY];
-
+        mapX=getRandomNumber(3,10);
+        mapY=getRandomNumber(3,10);
+        mapGrid=new String[mapX][mapY];
+        fillRooms();
     }
-    public class getSpawnRoom{
+    public static void getSpawnRoom(){
+        boolean loop=true;
+        int spawnX=0;
+        int spawnY=0;
+        while(loop==true){
+            spawnX=getRandomNumber(1,mapX);
+            spawnY=getRandomNumber(1,mapY);
+            if(isRoomEmpty(spawnX, spawnY)){
+                loop=false;
+            }
+            else{
+                loop=true;
+            }
+        }
+        mapGrid[spawnX][spawnY]="spawn";    
+    }
+    public static void getExitRoom(){
+        boolean loop=true;
+        int exitX=0;
+        int exitY=0;
+        while(loop==true){
+            exitX=getRandomNumber(1,mapX);
+            exitY=getRandomNumber(1,mapY);
+            if(isRoomEmpty(exitX, exitY)){
+                loop=false;
+            }
+            else{
+                loop=true;
+            }
+        }
+        mapGrid[exitX][exitY]="exit";
+    }
+    public static void getShop(){
+        boolean loop=true;
+        int shopX=0;
+        int shopY=0;
+        while(loop==true){
+            shopX=getRandomNumber(1,mapX);
+            shopY=getRandomNumber(1,mapY);
+            if(isRoomEmpty(shopX, shopY)){
+                loop=false;
+            }
+            else{
+                loop=true;
+            }
+        }
+        mapGrid[shopX][shopY]="shop";
+    }
+    public static int getRandomNumber(int min,int max){
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
         
+        return randomNum;
+        
+        // int returnNumber;
+        // Random rn=new Random();
+        // returnNumber=rn.nextInt(min)+(max-min);
+        // return returnNumber;
     }
-    public class getExitRoom{
-
+    public static boolean isRoomEmpty(int x, int y){
+        if(mapGrid[x][y]==""){   //this line gives an error Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 10
+            return true;
+        }
+        else{
+            return false;
+        } 
     }
-    public class fillRooms{
+    public static void fillRooms(){
+        getSpawnRoom();
+        getExitRoom();
+        getShop();
 
+        for(int x=0;x<mapX;x++){
+            for(int y=0;y<mapY;y++){
+                if(isRoomEmpty(x,y)){
+                    int roomDecider=getRandomNumber(1,4);
+                    switch(roomDecider){
+                        case 1: mapGrid[x][y]="monster";
+                        case 2: mapGrid[x][y]="loot";
+                        case 3: mapGrid[x][y]="chest";
+                        case 4: mapGrid[x][y]="standard";
+                    }
+                }
+            }
+        }
+        System.out.println(mapX+""+mapY);
     }
 }
